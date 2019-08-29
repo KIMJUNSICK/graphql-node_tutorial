23cc5cbd774700b0241c63d16d80ce1e8844cc92
Kim Jun Sik <jinaganda5@gmail.com>
2019-08-29 21:26:28 +0900

    Filter

--- src/api/Query/feed/feed.js
+++ src/api/Query/feed/feed.js
@@ -2,6 +2,20 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Query: {
-    feed: () => prisma.links()
+    feed: (_, args) => {
+      const { filter } = args;
+      return prisma.links({
+        where: {
+          OR: [
+            {
+              url_contains: filter
+            },
+            {
+              description_contains: filter
+            }
+          ]
+        }
+      });
+    }
   }
 };
