7b31c3c89598c4fae1d96f6236e6cb1d61a31f65
Kim Jun Sik <jinaganda5@gmail.com>
2019-08-29 21:33:20 +0900

    skip & first and Sorting

--- src/api/Query/feed/feed.js
+++ src/api/Query/feed/feed.js
@@ -3,7 +3,7 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Query: {
     feed: (_, args) => {
-      const { filter } = args;
+      const { filter, skip, first } = args;
       return prisma.links({
         where: {
           OR: [
@@ -14,7 +14,10 @@ export default {
               description_contains: filter
             }
           ]
-        }
+        },
+        skip,
+        first,
+        orderBy: "createdAt_DESC"
       });
     }
   }
